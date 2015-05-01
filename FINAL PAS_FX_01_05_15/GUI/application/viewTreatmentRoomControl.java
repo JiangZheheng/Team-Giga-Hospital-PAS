89f272c056e21acf0778c002657d334a3fe57903
clareotoole <cotoole06@qub.ac.uk>
2015-05-01 13:32:18 +0100

    Treatment room update

--- FINAL PAS_FX_01_05_15/GUI/application/viewTreatmentRoomControl.java
+++ FINAL PAS_FX_01_05_15/GUI/application/viewTreatmentRoomControl.java
@@ -3,14 +3,192 @@ package application;
 import java.net.URL;
 import java.util.ResourceBundle;
 
+import javafx.event.ActionEvent;
 import javafx.fxml.Initializable;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.TextArea;
+import javafx.scene.control.TextField;
 
-public class viewTreatmentRoomControl implements Initializable{
+public class viewTreatmentRoomControl {
 
-	@Override
-	public void initialize(URL location, ResourceBundle resources) {
-		
-		
-	}
+    @FXML
+    private TextField titleTextFieldT5;
+
+    @FXML
+    private TextField titleTextFieldT4;
+
+    @FXML
+    private TextField titleTextFieldT2;
+
+    @FXML
+    private TextField titleTextFieldT1;
+
+    @FXML
+    private Label treatmentRoom;
+
+    @FXML
+    private TextField firstNameTextFieldT1;
+
+    @FXML
+    private TextField firstNameTextFieldT4;
+
+    @FXML
+    private TextField firstNameTextFieldT5;
+
+    @FXML
+    private TextField firstNameTextFieldT2;
+
+    @FXML
+    private TextField firstNameTextFieldT3;
+
+    @FXML
+    private Label nhsNumberT12;
+
+    @FXML
+    private Label titleT1;
+
+    @FXML
+    private Label titleT2;
+
+    @FXML
+    private Label firstNameT5;
+
+    @FXML
+    private Label titleT4;
+
+    @FXML
+    private Label firstNameT4;
+
+    @FXML
+    private Label titleT5;
+
+    @FXML
+    private Label firstNameT1;
+
+    @FXML
+    private Label firstNameT2;
+
+    @FXML
+    private TextField nhsNumberTextFieldT3;
+
+    @FXML
+    private TextField nhsNumberTextFieldT4;
+
+    @FXML
+    private TextField nhsNumberTextFieldT1;
+
+    @FXML
+    private TextArea textBoxT4;
+
+    @FXML
+    private TextField nhsNumberTextFieldT2;
+
+    @FXML
+    private TextField nhsNumberTextFieldT5;
+
+    @FXML
+    private TextField lastNameTextFieldT4;
+
+    @FXML
+    private TextField lastNameTextFieldT5;
+
+    @FXML
+    private TextField lastNameTextFieldT2;
+
+    @FXML
+    private Label nhsNumberT1;
+
+    @FXML
+    private TextField lastNameTextFieldT3;
+
+    @FXML
+    private TextField lastNameTextFieldT1;
+
+    @FXML
+    private Label nhsNumberT4;
+
+    @FXML
+    private Label nhsNumberT5;
+
+    @FXML
+    private Label nhsNumberT2;
+
+    @FXML
+    private TextArea testBoxT12;
+
+    @FXML
+    private TextField triageCategoryT2;
+
+    @FXML
+    private TextArea testBoxT13;
+
+    @FXML
+    private TextField triageCategoryT3;
+
+    @FXML
+    private Button back;
+
+    @FXML
+    private TextField triageCategoryT4;
+
+    @FXML
+    private TextField triageCategoryT5;
+
+    @FXML
+    private Label treatmentRoom5;
+
+    @FXML
+    private Label treatmentRoom4;
+
+    @FXML
+    private Label firstNameT12;
+
+    @FXML
+    private Label triageCategoryT1;
+
+    @FXML
+    private Label lastNameT2;
+
+    @FXML
+    private Label lastNameT1;
+
+    @FXML
+    private Label lastNameT12;
+
+    @FXML
+    private Label lastNameT5;
+
+    @FXML
+    private Label lastNameT4;
+
+    @FXML
+    private Label treatmentRoom1;
+
+    @FXML
+    private Label treatmentRoom2;
+
+    @FXML
+    private Label triageCategoryT12;
+
+    @FXML
+    private TextArea testBoxT1;
+
+    @FXML
+    private TextArea testBoxT2;
+
+    @FXML
+    private Button logOut;
+
+    @FXML
+    void onClickBack(ActionEvent event) {
+
+    }
+
+    @FXML
+    void onClickLogOut(ActionEvent event) {
+
+    }
 
 }
