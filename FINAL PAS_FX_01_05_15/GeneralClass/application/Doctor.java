b47846f5c43b76ae843910ec90d78aa0183a3837
hannahmcdade <hmcdade01@qub.ac.uk>
2015-05-01 12:39:57 +0100

    Comments

--- FINAL PAS_FX_01_05_15/GeneralClass/application/Doctor.java
+++ FINAL PAS_FX_01_05_15/GeneralClass/application/Doctor.java
@@ -3,7 +3,9 @@ package application;
 import java.util.LinkedList;
 
 /**
- * Class to contain the details of the Doctor
+ * Class to contain the details of the Doctor, extending Staff Class, and
+ * including methods that the doctor can use throughout the system. Implements
+ * ILogin and ICategorise
  *
  */
 
@@ -15,7 +17,6 @@ public class Doctor extends Staff implements ILogin, ICategorise {
 	public Doctor() {
 
 	}
-	
 
 	/**
 	 * constructor with arguments for doctor Class
@@ -33,58 +34,53 @@ public class Doctor extends Staff implements ILogin, ICategorise {
 
 	}
 
-
 	/**
-	 * unimplemented method - to be used only in the NurseTriage Class
+	 * unimplemented method from ICategorise interface - only to be used by the
+	 * Triage Nurse, as a doctor can only alter the Triage category of a patient
+	 * 
+	 * @return
 	 */
 	@Override
-	public boolean categorisePatient(LinkedList<Patient> allPatients,LinkedList<Patient> patientQueue,  Patient patient, Triage triage)
+	public boolean categorisePatient(LinkedList<Patient> allPatients,
+			LinkedList<Patient> patientQueue, Patient patient, Triage triage)
 			throws HospitalPASException {
-		// creating an integer wrapper to all to check if the getTraigeCategory
-				// is a null value
-				Integer patientTriage = patient.getTriageCategory();
+		return false;
 
-				// if statement to check if the triage category has not been set - and
-				// if so sets triage category
-				if (patientTriage == 0) {
-					patient.setTriageCategory(triage);
-					// method to automatically add patient to queue once they are
-					// categorised by the triage nurse
-					if (putPatientIntoQueue(patientQueue, patient)) {
-						return true;
-					} else {
-						allPatients.remove(patient);
-						throw new HospitalPASException(
-								ExceptionsEnums.QUEUELIMITEXCEEDED.getException());
-						
-					}
-				} else {
-					throw new HospitalPASException(
-							ExceptionsEnums.PATIENTALREADYTRIAGED.getException());
-				}
-			}
+	}
 
-
+	/**
+	 * method to allow a doctor to alter the Triage Category of a patient should
+	 * their condition change - can be used in both the Treatment Rooms and the
+	 * Patient Queue
+	 */
 	@Override
-	public boolean recategorisePatient(LinkedList<Patient> patientQueue, Patient patient, Triage triage)
-			throws HospitalPASException {
+	public boolean recategorisePatient(LinkedList<Patient> patientQueue,
+			Patient patient, Triage triage) throws HospitalPASException {
+		// if statement to check that the patient is not a null value before assigning them a Triage category
 		if (patient != null) {
+			//sets the patient category
 			patient.setTriageCategory(triage);
 			// sorts the patient queue by triage category
-						patientQueue.sort(new SortPatientComparator());
+			patientQueue.sort(new SortPatientComparator());
 			return true;
 		} else {
-			throw new HospitalPASException(ExceptionsEnums.CANTRECOGNISEPATIENT.getException());
+			throw new HospitalPASException(
+					ExceptionsEnums.CANTRECOGNISEPATIENT.getException());
 		}
 
 	}
 
-
+	/**
+	 * Method to allow the Doctor to place the Patient back into the queue once
+	 * their triage category has be reassigned
+	 */
 	@Override
 	public boolean putPatientIntoQueue(LinkedList<Patient> patientQueue,
 			Patient patient) {
 		boolean inQueue = false;
 
+		// if statement to ensure that the queue limit has not been reached
+		// before the Doctor can place the patient into the queue
 		if (patientQueue.size() < Limits.PATIENT_LIMIT_IN_QUEUE) {
 			// adds patient to queue
 			patientQueue.add(patient);
