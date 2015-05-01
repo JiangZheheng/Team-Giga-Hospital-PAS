225294c1dca7f85b7ba402e14accf8e006f23db0
Finn <fintanmaguire88@gmail.com>
2015-05-01 13:14:20 +0100

    code clean up and comments added

--- FINAL PAS_FX_01_05_15/GeneralClass/application/OnCall.java
+++ FINAL PAS_FX_01_05_15/GeneralClass/application/OnCall.java
@@ -4,12 +4,24 @@ import java.util.LinkedList;
 
 public class OnCall {
 
+	/**
+	 * Boolean to say if on call team are engaged 
+	 */
 	private boolean onCallEngaged1=false;
 	
+	/**
+	 * Boolean to say if on call team are engaged 
+	 */
 	private boolean onCallEngaged2=false;
 
+	/**
+	 * Instatiation of the insitu object 
+	 */
 	private InSitu inSitu;
 
+	/**
+	 * Instantiation of the patient object 
+	 */
 	private Patient patient;
 
 
@@ -26,23 +38,42 @@ public class OnCall {
 		}
 	}
 	
+	/**
+	 * Method to set patient to be treated insitu 
+	 */
 	public void treatEmergency(){
 		inSitu.getPatient();
 		inSitu.setPatient(null);
 	}
 
+	/**
+	 * Boolean to get if on call team 1 currently engaged 
+	 * @return
+	 */
 	public boolean isOnCallEngaged1() {
 		return onCallEngaged1;
 	}
 
+	/**
+	 * Method to set if on call team 1 engaged 
+	 * @param onCallEngaged1
+	 */
 	public void setOnCallEngaged1(boolean onCallEngaged1) {
 		this.onCallEngaged1 = onCallEngaged1;
 	}
 
+	/**
+	 * Method to get is on call team 2 engaged 
+	 * @return
+	 */
 	public boolean isOnCallEngaged2() {
 		return onCallEngaged2;
 	}
 
+	/**
+	 * Method to set if on call team 2 engaged 
+	 * @param onCallEngaged2
+	 */
 	public void setOnCallEngaged2(boolean onCallEngaged2) {
 		this.onCallEngaged2 = onCallEngaged2;
 	}
