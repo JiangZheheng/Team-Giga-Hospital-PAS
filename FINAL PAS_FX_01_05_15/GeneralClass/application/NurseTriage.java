cc1ae7c62bdb82c1ca6b3410b7a221dcd37c57dc
jiangzheheng <809277527@qq.com>
2015-05-01 16:37:14 +0100

    update

--- FINAL PAS_FX_01_05_15/GeneralClass/application/NurseTriage.java
+++ FINAL PAS_FX_01_05_15/GeneralClass/application/NurseTriage.java
@@ -83,7 +83,6 @@ public class NurseTriage extends Staff implements INurseTriage,
 			if (putPatientIntoQueue(patientQueue, patient)) {
 				return true;
 			} else {
-				for()
 				allPatients.remove(patient);
 				
 
